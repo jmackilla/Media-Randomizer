15b77c3fd9e22d57c33cc6556360d769fb954633
Jordan <jordanmclemore@gmail.com>
2025-08-07 23:22:49 -0400

    cleaning runner

--- src/main/RunThemAll.java
+++ src/main/RunThemAll.java
@@ -2,9 +2,8 @@ package main;
 
 public class RunThemAll {
     public static void main(String[] args) {
-        String[] fakeargs = null;
-        SWFRandomizer.main(fakeargs);
-        AMVRandomizer.main(fakeargs);
-        MovieRandomizer.main(fakeargs);
+        SWFRandomizer.main(args);
+        AMVRandomizer.main(args);
+        MovieRandomizer.main(args);
     }
 }
