2171cdf5f79c2860fd27f8f96672c68ddfe76d93
Jordan <jordanmclemore@gmail.com>
2025-08-09 15:19:28 -0400

    optimizing code

--- src/component/Randomizer.java
+++ src/component/Randomizer.java
@@ -1,4 +1,4 @@
-package shared;
+package component;
 
 import javax.swing.*;
 import java.awt.*;
@@ -9,43 +9,33 @@ import java.util.Arrays;
 import java.util.Objects;
 import java.util.Random;
 
-
+/**
+ * Single directory file randomizer with GUI
+ */
 public class Randomizer {
 
-    // TODO: add keybindings to play next file
     private final int topMargin;
+    private final boolean showExit;
     ArrayList<File> fileList;
-    private final JFrame frame = new JFrame();
     private File previousFile;
+
+    public Randomizer(String path, int topMargin, boolean showExit) {
+        this.fileList = new ArrayList<>(Arrays
+                .asList(Objects.requireNonNull(new File(path).listFiles())));
+        this.topMargin = topMargin;
+        this.showExit = showExit;
+    }
 
     public Randomizer(String path, int topMargin) {
         this.fileList = new ArrayList<>(Arrays
                 .asList(Objects.requireNonNull(new File(path).listFiles())));
-        this.topMargin = topMargin;        
+        this.topMargin = topMargin;
+        this.showExit = false;
     }
 
+
     public void init() {
-        //TODO: hide button boarders
-
-        frame.setSize(599, 75); // not the real size, but a hack, see below
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.getRootPane().setWindowDecorationStyle(JRootPane.NONE); // good if you want no bar
-
-        // other bars
-/*        "JRootPane.NONE",
-                "JRootPane.FRAME",
-                "JRootPane.PLAIN_DIALOG",
-                "JRootPane.INFORMATION_DIALOG",
-                "JRootPane.ERROR_DIALOG",
-                "JRootPane.COLOR_CHOOSER_DIALOG",
-                "JRootPane.FILE_CHOOSER_DIALOG",
-                "JRootPane.QUESTION_DIALOG",
-                "JRootPane.WARNING_DIALOG"*/
-
-        frame.setUndecorated(true);
-        frame.setVisible(true);
-        frame.setBounds(0, topMargin, 600, 40);
-
+        
         JPanel buttonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
         buttonPanel.setBackground(Color.black);
 
@@ -92,18 +82,49 @@ public class Randomizer {
 
         buttonPanel.add(currentButton);
 
-        JButton closeButton = new JButton("Exit");
-        closeButton.setBackground(Color.BLACK);
-        closeButton.setForeground(Color.GRAY);
-        buttonPanel.add(closeButton);
-        closeButton.addActionListener(e -> {
-            try {
-                frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
-            } catch (Exception ex) {
-                throw new RuntimeException(ex);
-            }
-        });
+        Frame frame = createFrame();
+        if (showExit) {
+            JButton closeButton = new JButton("Exit");
+            closeButton.setBackground(Color.BLACK);
+            closeButton.setForeground(Color.GRAY);
+            buttonPanel.add(closeButton);
+            closeButton.addActionListener(e -> {
+                try {
+                    frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
+                } catch (Exception ex) {
+                    throw new RuntimeException(ex);
+                }
+            });
+        }
+
         frame.add(buttonPanel);
+
+    }
+
+    public JFrame createFrame() {
+        JFrame frame = new JFrame();
+
+        frame.setSize(600, 75); // not the real size, but a hack, see below
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.getRootPane().setWindowDecorationStyle(JRootPane.NONE); // good if you want no bar
+
+        //TODO: hide button boarders
+        // other bars
+/*        "JRootPane.NONE",
+                "JRootPane.FRAME",
+                "JRootPane.PLAIN_DIALOG",
+                "JRootPane.INFORMATION_DIALOG",
+                "JRootPane.ERROR_DIALOG",
+                "JRootPane.COLOR_CHOOSER_DIALOG",
+                "JRootPane.FILE_CHOOSER_DIALOG",
+                "JRootPane.QUESTION_DIALOG",
+                "JRootPane.WARNING_DIALOG"*/
+
+        frame.setUndecorated(true);
+        frame.setVisible(true);
+        frame.setBounds(0, topMargin, 600, 40);
+
+        return frame;
     }
 
     public File openSwfAndReturnName() throws Exception {
---
+++ src/main/MediaRandomizer.java
@@ -0,0 +1,34 @@
+package main;
+
+import component.Randomizer;
+
+public class MediaRandomizer {
+
+public static final int ROW_ONE = 0;
+
+public static final int ROW_TWO = 40;
+
+public static final int ROW_THREE = 80;
+
+public static final int ROW_FOUR = 120;
+
+public static final int ROW_GIVE = 160;
+
+    public static void main(String[] args) {
+
+        Randomizer randomizer = new Randomizer("W:\\STUFF FOLDER\\lel", ROW_ONE);
+        randomizer.init();
+        
+        Randomizer randomizer2 = new Randomizer("W:\\STUFF FOLDER\\amv", ROW_TWO);
+        randomizer2.init();
+
+        Randomizer randomizer3 = new Randomizer("W:\\STUFF FOLDER\\funny", ROW_THREE);
+        randomizer3.init();
+
+        Randomizer randomizer4 = new Randomizer("W:\\STUFF FOLDER\\Clover videos", ROW_FOUR);
+        randomizer4.init();
+
+        Randomizer randomizer5 = new Randomizer("W:\\Videos", ROW_GIVE, true);
+        randomizer5.init();
+    }
+}
